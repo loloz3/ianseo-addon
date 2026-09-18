8421b6ad54d41186af2c6ce5434a2a183ea5df18
loloz3 <loloz3@free.fr>
2026-09-18 10:29:53 +0200

    Delete AutresTirs directory
    Removed: feature now integrated into IANSEO/FFTA.

--- AutresTirs/menu.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!empty($on) && isset($ret['PRNT'])) {
-    array_splice($ret['PRNT'], 6, 0, 'Impression Autres Tirs' . '|' . $CFG->ROOT_DIR . 'Modules/Custom/AutresTirs/' . 'PrnAutresTirs.php');
-}
-
-if (!empty($on) && isset($ret['QUAL'])) {
-    array_splice($ret['QUAL'], 12, 0, 'Impression Autres Tirs' . '|' . $CFG->ROOT_DIR . 'Modules/Custom/AutresTirs/' . 'PrnAutresTirs.php');
-}
